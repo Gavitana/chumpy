63a639b8d76b8b719ea1504f658fa942b7fa29be
Виталий Гайнутдинов <vitalya.gaynutdinov@gmail.com>
2022-12-14 11:31:30 +0700

    change setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ from distutils.core import setup
 #except AttributeError:  # for pip >= 20.1
 #    install_requires = [str(ir.requirement) for ir in install_reqs]
 #
-def get_version():
-    namespace = run_path('chumpy/version.py')
-    return namespace['version']
+# def get_version():
+#     namespace = run_path('chumpy/version.py')
+#     return namespace['version']
 
 setup(name='chumpy',
-    version=get_version(),
+    # version=get_version(),
     packages = ['chumpy'],
     author='Matthew Loper',
     author_email='matt.loper@gmail.com',
